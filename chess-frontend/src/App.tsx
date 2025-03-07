eb5c42f91365e82fc98374ca7427fe8d381017f7
Tusharkumar888 <tushakumar888219@gmail.com>
2025-03-08 01:18:59 +0530

    add the tailwind

--- chess-frontend/src/App.tsx
+++ chess-frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,6 +8,9 @@ function App() {
 
   return (
     <>
+      <div className=''>
+         hello world my name is tushar
+      </div>
 
     </>
   )
